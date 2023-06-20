ee54db4d85700d6e6a41dca4bb185ee1468d1b37
Dallas Moore <moore.johndallas@gmail.com>
2023-06-20 10:49:50 -0500

    Started code for homeRoute

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,36 @@
+const homeRouter = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+homeRouter.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+);
+
+module.exports = homeRouter;
+
+
+
+
+
+
+
+
