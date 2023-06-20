2882d00b8fb4fa653081399e4fe4253feeb6c66b
Dallas Moore <moore.johndallas@gmail.com>
2023-06-19 19:45:00 -0500

    Added code to post model

---
+++ models/Post.js
@@ -0,0 +1,41 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true, 
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        modelName: 'Post'
+    }
+);
+
+module.exports = Post;
