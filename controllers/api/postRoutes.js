3864c9ca23b74143005037ce232655dd10f87023
Dallas Moore <moore.johndallas@gmail.com>
2023-06-28 00:37:08 -0500

    Added post view and js file, seperated login/signup, adjusted routes accordingly

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,26 +3,34 @@ const { Post, User, Comment } = require('../../models');
 
 //render new post view with form
 postRouter.get('/new', (req, res) => {
-    res.render('new-post'), {
-        loggedIn: req.session.loggedIn
+    res.render('post'), {
+        logged_in: req.session.logged_in
     };
 }
 );
 
 // Post request to add post to database upon form submission
-postRouter.post('/', async (req, res) => {
+postRouter.post('/new', async (req, res) => {
     try {
         const newPost = await Post.create({
             title: req.body.title,
             content: req.body.content,
-            user_id: req.session.user_id,
+            // Figure out why this doesnt work
+            // user_id: req.session.user_id,
+            user_id: 1,
         });
-
+console.log(newPost);
         res.status(200).json(newPost);
+        res.render('profile'), {
+            logged_in: req.session.logged_in
+        };
     } catch (err) {
         res.status(400).json(err);
     }
 }
+
+
+
 );
 
 module.exports = postRouter;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -56,6 +56,44 @@ homeRouter.get('/login', (req, res) => {
 }
 );
 
+homeRouter.get('/signup', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
+}
+);
+
+// homeRouter.get('/dashboard', withAuth, async (req, res) => {
+//     try {
+//         const postData = await Post.findAll({
+//             where: {
+//                 user_id: req.session.user_id,
+//             },
+//             include: [
+//                 {
+//                     model: User,
+//                     attributes: ['username'],
+//                 },
+//             ],
+//         });
+
+//         const posts = postData.map((post) => post.get({ plain: true }));
+
+//         res.render('dashboard', {
+//             posts,
+//             logged_in: req.session.logged_in,
+//         });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// }
+// );
+
+
+
 
 
 module.exports = homeRouter;
